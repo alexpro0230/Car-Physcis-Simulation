f9ad0ce9a81ffc8c7cf59e5f37412141433b385f
alexpro0230 <77921445+alexpro0230@users.noreply.github.com>
2021-12-17 18:40:19 +0100

    some more things

--- Assets/Scripts/CarPhysics.cs
+++ Assets/Scripts/CarPhysics.cs
@@ -3,7 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CarPhysics : MonoBehaviour {
-    // Forces
+    #region forces
+
     public Vector3 Ftraction;
-    
+    public Vector3 Fdrag;
+    public Vector3 Frr;
+    public Vector3 Flong;
+    #endregion
+
+    #region vectors
+
+    public Vector3 u;
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    #endregion
+
+    #region Constants
+
+    public float EngineForce;
+    public float CarMass;
+    #endregion
+
+    private void Start() {
+        velocity = 
+            u = 
+            acceleration =
+            Fdrag =
+            Frr =
+            Flong =
+            new Vector3();
+    }
+
+
+    private void Update() {
+        
+    }
 }
