23c7676190a233ad224f02ce5238f77107ca37a7
alexpro0230 <77921445+alexpro0230@users.noreply.github.com>
2021-12-17 19:52:31 +0100

    more changes

--- Assets/Scripts/CarPhysics.cs
+++ Assets/Scripts/CarPhysics.cs
@@ -11,6 +11,8 @@ public class CarPhysics : MonoBehaviour {
     public Vector3 Flong;
     #endregion
 
+    [Header("\n\n")]
+
     #region vectors
 
     public Vector3 u;
@@ -18,13 +20,27 @@ public class CarPhysics : MonoBehaviour {
     public Vector3 acceleration;
     #endregion
 
+    [Header("\n\n")]
+
+
     #region Constants
 
     public float EngineForce;
     public float CarMass;
+
+
+    public float Cdrag;
+    public float Crr;
+
+    public bool SetDefaultConstants;
     #endregion
 
     private void Start() {
+        
+        //foreach (Transform child in transform) {
+        //    child.gameObject.GetComponent<WheelCollider>().motorTorque = 500;
+        //}
+        
         velocity = 
             u = 
             acceleration =
@@ -32,10 +48,25 @@ public class CarPhysics : MonoBehaviour {
             Frr =
             Flong =
             new Vector3();
+
+        if(SetDefaultConstants) {
+            Cdrag = 0.5f * 0.3f * 2.2f * 1.29f; // 0.4257
+            Crr = 30 * Cdrag;
+        }
+
+        rb = GetComponent<Rigidbody>();
     }
 
-
+    Rigidbody rb;
     private void Update() {
-        
+        Vector2 inputDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        u = transform.forward * inputDirection.y + transform.right * inputDirection.x;
+
+        Ftraction = u * EngineForce;
+        Fdrag = -Cdrag * rb.velocity * rb.velocity.magnitude;
+        Frr = -Crr * rb.velocity;
+        Flong = Ftraction + Fdrag + Frr;
+        acceleration = Flong;
     }
 }
